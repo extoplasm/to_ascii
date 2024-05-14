d381fc581917e3cd4984cae5effe1e640fbf15c4
extoplasm <pleassehelsp@gmail.com>
2024-05-14 19:43:05 +1000

    l

--- main.py
+++ main.py
@@ -17,5 +17,27 @@ class to_ascii():
                     print(self.ascii_values[2])
                 elif self.px[x,y] < 68:
                     print(self.ascii_values[3])
+                elif self.px[x,y] < 85:
+                    print(self.ascii_values[4])
+                elif self.px[x,y] < 102:
+                    print(self.ascii_values[5])
+                elif self.px[x,y] < 119:
+                    print(self.ascii_values[6])
+                elif self.px[x,y] < 136:
+                    print(self.ascii_values[7])
+                elif self.px[x,y] < 153:
+                    print(self.ascii_values[8])
+                elif self.px[x,y] < 170:
+                    print(self.ascii_values[9])
+                elif self.px[x,y] < 187:
+                    print(self.ascii_values[10])
+                elif self.px[x,y] < 204:
+                    print(self.ascii_values[11])
+                elif self.px[x,y] < 221:
+                    print(self.ascii_values[12])
+                elif self.px[x,y] < 238:
+                    print(self.ascii_values[13])
+                else:
+                    print(self.ascii_values[14])
 
 to_ascii("./idle.gif").convert()
